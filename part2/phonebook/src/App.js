646fdf340ef8920e78b96562fee733e8e8e60a49
sukalov <matvey10@gmail.com>
2023-04-23 23:14:53 +0300

    made number input required

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -17,12 +17,12 @@ const App = () => {
     const personsCopy = [...persons]
     const personIndex = persons.indexOf(filter)
     if (filter) {
-      if (filter.number === newNumber || (newNumber === '' && newName != '')) {
+      if (filter.number === newNumber || newNumber === '') {
         alert(`${newName}'s already in contacts with this number`)
     } else if (window.confirm(`Are you sure you want to change the number for ${newName} from ${filter.number} to ${newNumber}`)) {
      personsCopy[personIndex] = {name: filter.name, number: newNumber, id: filter.id}
      setPersons(personsCopy)}
-  } else if (newName !== '') {
+  } else {
     setPersons([...persons, {name: newName, number: newNumber, id: persons.length + 1}])
   }
     setNewName('')
@@ -38,15 +38,15 @@ const App = () => {
     <div>
       <h1>Phonebook</h1>
       <div>
-          search for <input type='text' onChange={handleSearchChange} value={search}/>
+          search for <input type='text' onChange={handleSearchChange} value={search} />
       </div>
       <h3>Create new contact</h3>
       <form onSubmit={addPerson}>
         <div>
-          name: <input type='text' onChange={handleNameChange} value={newName}/>
+          name: <input type='text' onChange={handleNameChange} value={newName} required/>
         </div>
         <div>
-          number: <input type='text' onChange={handleNumberChange} value={newNumber}/>
+          number: <input type='text' onChange={handleNumberChange} value={newNumber} required/>
         </div>
         <div>
           <br /><button type="submit">Add person</button>
