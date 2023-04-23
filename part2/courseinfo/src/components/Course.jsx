c4c0ac94016c6e695aa357ff2c43bd05b2779b71
sukalov <matvey10@gmail.com>
2023-04-23 14:13:32 +0300

    2.4. allows multiple courses

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -2,12 +2,18 @@ import Header from './Header'
 import Content from './Content'
 import Total from './Total'
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
     return (
         <div>
-          <Header course={course.name} />
-          <Content parts={course.parts} />
-          <Total parts = {course.parts} />
+          {courses.map(course => {
+            return (
+              <>
+                <Header course={course.name} />
+                <Content parts={course.parts} />
+                <Total parts = {course.parts} />
+              </>
+            )
+          })}
         </div>
       )
 }
