ef4397374b4ec64a6d24ca0dccdb3aa44e48f7c2
sukalov <matvey10@gmail.com>
2023-04-23 14:14:04 +0300

    keys added

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -7,11 +7,11 @@ const Course = ({ courses }) => {
         <div>
           {courses.map(course => {
             return (
-              <>
+              <div key={course.id}>
                 <Header course={course.name} />
                 <Content parts={course.parts} />
                 <Total parts = {course.parts} />
-              </>
+              </div>
             )
           })}
         </div>
