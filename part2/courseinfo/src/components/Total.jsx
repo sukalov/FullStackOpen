8bad3ee5777320aaec087c8a294d30783ca4ee91
sukalov <matvey10@gmail.com>
2023-04-23 14:08:26 +0300

    .

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,7 +1,7 @@
 const Total = ({ parts }) => (
     <h4>
         Total: {parts.reduce(
-                    (sum, part) => 
+                    (sum, part) =>  
                     sum + part.exercises, 0)
                 }
     </h4>
