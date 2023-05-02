24ed594f41044e619a3af4ab5f22fd2a627dc5ce
sukalov <matvey10@gmail.com>
2023-05-02 08:13:07 +0300

    part d completed.  2-12 - 2-15

--- part2/phonebook/src/components/ContactList.jsx
+++ part2/phonebook/src/components/ContactList.jsx
@@ -1,4 +1,19 @@
-const ContactList = ({ persons, search }) => {
+import Button from "./Button"
+import dbService from "../services/dbService"
+
+const ContactList = ({ persons, search, set }) => {
+    const personsUpd = [...persons]
+    const deleteContact = (person) => {
+        const index = personsUpd.indexOf(person)
+        if (window.confirm(`Are you sure you want to delete ${person.name} from conacts?`)) {
+            dbService.del(person)
+                .then(res => {
+                    personsUpd.splice(index);
+                    set(personsUpd);
+                })
+                .catch(e => console.log(e))
+        }
+    }
     return (
         <>
             <h3>Contacts</h3>
@@ -13,6 +28,7 @@ const ContactList = ({ persons, search }) => {
                 <tr key={person.id}>
                     <td> {person.name}</td>
                     <td> {person.number}</td>
+                    <td><Button text="delete" onClick={() => deleteContact(person)}/></td>
                 </tr>
             )}
             </tbody>
