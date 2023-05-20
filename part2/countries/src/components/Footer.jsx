0af75a2e381af0690c9217837af6f06dbb81b8d1
sukalov <matvey10@gmail.com>
2023-05-20 14:26:49 -0600

    footer info upd

--- part2/countries/src/components/Footer.jsx
+++ part2/countries/src/components/Footer.jsx
@@ -3,7 +3,8 @@ const Footer = () => {
         <footer>
             <p>Main data provided by <a href="https://www.restcountries.com/" title="Countries REST API">RestCountries.com</a></p>
             <p>Powered by <a href="https://www.weatherapi.com/" title="Free Weather API">WeatherAPI.com</a></p>
-            <p><a href='https://github.com/sukalov/FullStackOpen/tree/main/part2/countries'>Project source code</a></p>
+            <p>View source code on <a href='https://github.com/sukalov/FullStackOpen/tree/main/part2/countries'>Github</a></p>
+            <p>Created as a test project for <a href="https://www.fullstackopen.com/" title="FullStackOpen free Web Development course">FullStackOpen</a></p>
         </footer>
     )
 }
