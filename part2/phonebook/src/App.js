82a387769f7c67f076292ac3017b0f67284cb108
sukalov <matvey10@gmail.com>
2023-05-30 21:27:00 -0600

    trim number

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   },[])
 
   useEffect(() => {
-    let parsedNumber = newNumber.replace(/[())\s-]*/g, '')
+    let parsedNumber = newNumber.replace(/[())\s-]*/g, '').trim()
     if (parsedNumber.length === 10 && !isNaN(Number(parsedNumber))) {
       setNumberValidation(true)
     } else {setNumberValidation(false)}
