12dbd580decc377e37af4338cb357bde9430c3a9
sukalov <matvey10@gmail.com>
2023-06-01 09:32:58 -0600

    4.5 + 4.6 author with most blogs (tests passed)

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -7,8 +7,106 @@
 
 const listHelper = require('../utils/list_helper')
 
+const blogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0,
+  },
+]
+
 test('dummy returns one', () => {
-  const blogs = []
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('of []', () => {
+    expect(listHelper.totalLikes([])).toBe(0)
+  })
+  test('of [{likes: 4}]', () => {
+    expect(listHelper.totalLikes([{ likes: 4 }])).toBe(4)
+  })
+  test('of [{likes: 4}, {likes: 2}]', () => {
+    expect(listHelper.totalLikes([{ foo: 'bar', likes: 4 }, { likes: 2 }])).toBe(6)
+  })
+  test('of dummy blogs sums likes correctly', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(36)
+  })
+})
+
+describe('favourite blog', () => {
+  test('of []', () => {
+    expect(listHelper.favoriteBlog([])).toEqual(undefined)
+  })
+  test('of [{likes: 4}]', () => {
+    expect(listHelper.favoriteBlog([{ likes: 4 }])).toEqual({ likes: 4 })
+  })
+  test('of [{likes: 4}, {likes: 2}]', () => {
+    expect(listHelper.favoriteBlog([{ foo: 'bar', likes: 4 }, { likes: 2 }])).toEqual({ foo: 'bar', likes: 4 })
+  })
+  test('of dummy blogs best result is 12', () => {
+    expect(listHelper.favoriteBlog(blogs)).toEqual(blogs[2])
+  })
+})
+
+describe('most blogs', () => {
+  test('of [] equals {}', () => {
+    expect(listHelper.mostBlogs([])).toEqual({})
+  })
+  test('of 1 test blogpost', () => {
+    expect(listHelper.mostBlogs([{ author: 'test', likes: 4 }])).toEqual({ author: 'test', blogs: 1 })
+  })
+  test('of bloglist with no authors', () => {
+    expect(listHelper.mostBlogs([{ foo: 'bar', likes: 4 }, { likes: 2 }])).toEqual({ author: 'undefined', blogs: 2 })
+  })
+  test('of bloglist where 1 authored post and many anonymous', () => {
+    expect(listHelper.mostBlogs([{ foo: 'bar', likes: 4 }, { likes: 2 }, { author: 'test', likes: 4 }])).toEqual({ author: 'test', blogs: 1 })
+  })
+  test('of dummy blogs where best result is Robert C. Martin', () => {
+    expect(listHelper.mostBlogs(blogs)).toEqual({ author: 'Robert C. Martin', blogs: 3 })
+  })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,6 +4,36 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => (
+  blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+)
+
+const favoriteBlog = (blogs) => (
+  blogs.length === 0
+    ? undefined
+    : blogs.reduce((top, blog) => (
+      blog.likes > top.likes
+        ? blog
+        : top
+    ))
+)
+
+const mostBlogs = (blogs) => {
+  const likes = blogs.reduce((acc, blog) => {
+    acc[blog.author] = acc[blog.author] + 1 || 1
+    return acc
+  }, {})
+  return Object.keys(likes).reduce((acc, author) => (
+    (toString(author) === 'undefined' && acc.blogs > likes[author])
+      ? acc
+      : { author, blogs: likes[author] }), {})
+}
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
 }
