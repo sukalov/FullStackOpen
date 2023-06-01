44bc091eef90ed122aaefd20a52a7d51352a1ad6
sukalov <matvey10@gmail.com>
2023-05-31 22:39:48 -0600

    couple eslint comments

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express'
 import cors from 'cors'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import morgan from 'morgan'
 import appRouter from './controllers/bloglist.js'
 
