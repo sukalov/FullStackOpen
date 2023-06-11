334dacb101d11d39e20ac8a7d799698eb2ac9f48
sukalov <matvey10@gmail.com>
2023-06-11 01:31:35 -0600

    .

--- part5/bloglist-frontend/src/components/Login.jsx
+++ part5/bloglist-frontend/src/components/Login.jsx
@@ -1,10 +1,16 @@
 const Login = ({ user, username, setUsername, password, setPassword, handleLogin }) => {
     return (
     <div className="flex h-screen items-center p-2">
-        <form onSubmit={handleLogin} className="px-8 py-6 rounded-md bg-orange-50 dark:bg-stone-600 shadow-md flex mx-auto flex-1 flex-col items-stretch max-w-sm">
+        <form onSubmit={handleLogin} className="px-8 py-6 rounded-md bg-orange-50 dark:bg-stone-600 shadow-2xl dark:shadow-xl flex mx-auto flex-1 flex-col items-stretch max-w-sm">
             <div className="input-grid">
-                <p className="dark:text-orange-50 my-1">username</p>
-                <input className="px-2 dark:bg-neutral-700 w-full block border border-stone-300 dark:border-stone-800 p-1 rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-600 focus-visible:ring-offset-0 dark:focus-visible:ring-orange-300 dark:text-orange-50"
+                <label for="username">
+                    <p className="dark:text-orange-50 my-1 text-neutral-600">
+                        username
+                    </p>
+                </label>
+                <div className="relative">
+
+                    <input className="px-2 dark:bg-neutral-700 w-full block border border-stone-300 dark:border-stone-800 p-1 rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-600 focus-visible:ring-offset-0 dark:focus-visible:ring-orange-300 dark:text-orange-50"
                     id="username"
                     type="text"
                     value={username}
@@ -13,9 +19,14 @@ const Login = ({ user, username, setUsername, password, setPassword, handleLogin
                     required
                     autoComplete="off"
                 />
+                </div>
             </div>
             <div>
-                <p className="dark:text-orange-50 mt-2 mb-1">password</p>
+            <label for="password">
+                    <p className="dark:text-orange-50 mb-1 mt-2 text-neutral-600 ">
+                        password
+                    </p>
+                </label>
                 <input
                     className="px-2 dark:bg-neutral-700 border  border-stone-300 dark:border-stone-800 w-full p-1 rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-orange-600 dark:focus-visible:ring-orange-300 focus-visible:ring-offset-0 dark:text-orange-50"
                     type="password"
