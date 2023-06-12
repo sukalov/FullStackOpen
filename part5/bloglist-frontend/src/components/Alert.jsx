ba155627c0af56c7b3fa3c6ca049d6cbf74e50a8
sukalov <matvey10@gmail.com>
2023-06-12 01:32:43 -0600

    successful alert icon

--- part5/bloglist-frontend/src/components/Alert.jsx
+++ part5/bloglist-frontend/src/components/Alert.jsx
@@ -1,14 +1,14 @@
-import { AlertTriangle } from "lucide-react"
+import { AlertTriangle, CheckCircle } from "lucide-react"
 
 const Alert = ({ event }) => {
     return (
         event.status === 'bad' ?
         <div className="fixed md:right-2 md:bottom-3 border border-red-500 bg-red-300 rounded-md px-4 py-2 dark:bg-opacity-20 animate-bounce-in dark:text-red-200">
-            <AlertTriangle size={22} strokeWidth={1.25} className="inline mb-1"/>
+            <AlertTriangle size={18} strokeWidth={1.5} className="inline mb-1"/>
             <p className="inline mx-2 font-light">{event.message}</p>
         </div> :
         <div className="fixed right-2 bottom-3 border border-green-500 bg-green-300 rounded-md px-4 py-2 dark:bg-opacity-20 animate-bounce-in dark:text-green-200">
-            <AlertTriangle size={22} strokeWidth={1.25} className="inline mb-1"/>
+            <CheckCircle size={18} strokeWidth={1.5} className="inline mb-0.5"/>
             <p className="inline mx-2 font-light">{event.message}</p>
         </div> 
 
